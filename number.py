c40d991d379e68154e1c56ea33572a31787d6fc9
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-14 23:48:34 +0530

    Update number.py

--- number.py
+++ number.py
@@ -1,19 +1,19 @@
 #funcitions related to numbers in python
 #***MATH MODULE***
 
-#import math
-#pi=3.14
-#x=1
-#y=4
-#z=9
+import math
+pi=3.14
+x=1
+y=4
+z=9
 
 
-#print(round(pi))
-#print(math.ceil(pi))
-#print(math.floor(pi))
-#print(abs(pi))
-#print(pow(pi,5))
-#print(math.sqrt(pi))
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(pow(pi,5))
+print(math.sqrt(pi))
 
-#print(max(x,y,z))
-#print(min(x,y,z))
+print(max(x,y,z))
+print(min(x,y,z))
